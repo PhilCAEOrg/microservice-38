e49d35f55a46bd6fbb3d2546f62c07d525b837b3
PhilCAEUser <rbyETE4Ceu2iA4Z>
2020-07-11 12:21:42 +0000

    Test

---
+++ db/38_create_tables.sql
@@ -0,0 +1,16 @@
+--
+-- Database Schema:  caeschema
+-- Automatically generated sql script for the service 38, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table tab.
+--
+CREATE TABLE caeschema.tab (
+  id INTEGER ,
+CONSTRAINT id_PK PRIMARY KEY (id)
+ 
+);
+
+
+
